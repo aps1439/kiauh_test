f0818e3223e15be6c4b7012ef0ace54565d33bca
aps1439 <103040873+aps1439@users.noreply.github.com>
2024-07-30 09:57:08 +0200

    edit line 41

--- scripts/globals.sh
+++ scripts/globals.sh
@@ -38,7 +38,7 @@ function set_globals() {
   #================= MOONRAKER ==================#
   MOONRAKER_ENV="${HOME}/moonraker-env"
   MOONRAKER_DIR="${HOME}/moonraker"
-  MOONRAKER_REPO="https://github.com/aps1439/moonraker_test.git@v0.9.1"
+  MOONRAKER_REPO="https://github.com/Arksine/moonraker.git@v0.9.1"
 
   #================= MAINSAIL ===================#
   MAINSAIL_DIR="${HOME}/mainsail"
